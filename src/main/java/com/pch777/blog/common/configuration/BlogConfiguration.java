6a160488a08204d6405afba79831039b2bfbacff
Pawel Chmiel-Kozdranski <pch777@gazeta.pl>
2024-07-28 09:29:45 +0200

    Update BlogConfiguration, BlogCommonViewController

--- src/main/java/com/pch777/blog/common/configuration/BlogConfiguration.java
+++ src/main/java/com/pch777/blog/common/configuration/BlogConfiguration.java
@@ -24,9 +24,45 @@ public class BlogConfiguration {
     @Value("${blog.article.archive.numberOfLastMonths}")
     private int numberOfLastMonths;
 
+    @Value("${blog.paging.pageSize}")
+    private int pageSize;
+
     @Value("${blog.article.paging.pageSize}")
     private int articlesPageSize;
 
     @Value("${blog.comment.paging.pageSize}")
     private int commentsPageSize;
+
+    @Value("${blog.admin.category.paging.pageSize}")
+    private int adminCategoriesPageSize;
+
+    @Value("${blog.user.defaultAvatarUrl}")
+    private String defaultAvatarUrl;
+
+    @Value("${blog.verification.token.verificationTime}")
+    private long tokenVerificationTime;
+
+    @Value("${blog.verification.view.baseUrl}")
+    private String verificationViewBaseUrl;
+
+    @Value("${blog.verification.api.baseUrl}")
+    private String verificationApiBaseUrl;
+
+    @Value("${blog.password.reset.verificationTime}")
+    private long passwordVerificationTime;
+
+    @Value("${blog.password.reset.baseUrl}")
+    private String passwordBaseUrl;
+
+    @Value("${blog.mail.subject.verification.email}")
+    private String subjectVerificationEmail;
+
+    @Value("${blog.mail.subject.reset.password}")
+    private String subjectResetPassword;
+
+    @Value("${blog.mail.from.address}")
+    private String fromAddress;
+
+    @Value("${blog.mail.from.name}")
+    private String fromName;
 }
--- src/main/java/com/pch777/blog/common/controller/BlogCommonViewController.java
+++ src/main/java/com/pch777/blog/common/controller/BlogCommonViewController.java
@@ -1,9 +1,12 @@
 package com.pch777.blog.common.controller;
 
 import com.pch777.blog.article.service.ArticleService;
+import com.pch777.blog.article.service.UserArticleLikeService;
 import com.pch777.blog.category.service.CategoryService;
 import com.pch777.blog.tag.service.TagService;
+import com.pch777.blog.identity.user.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.ui.Model;
 
 @AllArgsConstructor
@@ -12,11 +15,18 @@ public abstract class BlogCommonViewController {
     protected CategoryService categoryService;
     protected ArticleService articleService;
     protected TagService tagService;
+    protected UserService userService;
+    protected UserArticleLikeService userArticleLikeService;
 
-    protected void addGlobalAttributes(Model model) {
+    protected void addGlobalAttributes(Model model, UserDetails userDetails) {
+        if (userDetails != null) {
+            model.addAttribute("avatarUrl", userService.getUserByUsername(userDetails.getUsername()).getAvatarUrl());
+            model.addAttribute("userId", userService.getUserByUsername(userDetails.getUsername()).getId());
+        }
         model.addAttribute("categories", categoryService.getCategories());
         model.addAttribute("monthYearList", articleService.getNumberOfLastMonths());
         model.addAttribute("tags", tagService.get10TagsByPopularity());
         model.addAttribute("top3articles", articleService.getTop3PopularArticles());
+        model.addAttribute("userArticleLikeService", userArticleLikeService);
     }
 }
