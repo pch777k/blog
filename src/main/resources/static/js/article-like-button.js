31005c9570af010230111b2e5447db868e7f5f38
Pawel Chmiel-Kozdranski <pch777@gazeta.pl>
2024-08-01 10:15:15 +0200

    refactor view pages

--- src/main/resources/static/js/article-like-button.js
+++ src/main/resources/static/js/article-like-button.js
@@ -1,19 +1,18 @@
-$(document).ready(function () {
-    $(".likeButton").click(function () {
-        let articleId = $(this).data("article-id");
-        let likesElement = $('#likesCount-' + articleId);
-        $.ajax({
-            type: "POST",
-            url: "/api/v1/articles/" + articleId + "/like",
-            success: function () {
-                let currentLikes = parseInt(likesElement.text());
-                likesElement.text(currentLikes + 1);
-                console.log("Likes increased successfully!");
-            },
-            error: function (xhr, status, error) {
-                console.error("Error while increasing likes:", error);
-            }
-        });
+$(document).on('click', '.likeButton', function () {
+    let articleId = $(this).data("article-id");
+    let likesElement = $('#likesCount-' + articleId);
+    $.ajax({
+        type: "POST",
+        url: "/api/v1/articles/" + articleId + "/like",
+        success: function (response) {
+            console.log("Server response:", response);
+            likesElement.text(response.likesCount);
+            console.log("Likes increased successfully!");
+        },
+        error: function (xhr, status, error) {
+            console.error("Error while increasing likes:", error);
+            console.error("Response text:", xhr.responseText);
+        }
     });
 });
 
